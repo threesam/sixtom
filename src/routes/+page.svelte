9b1d51d99f1263ff6f042d22290a34b1664af061
threesam <salvatoredangelo@protonmail.com>
2023-01-06 23:52:06 -0500

    splash

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,32 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<svelte:head>
+	<title>sixtom</title>
+</svelte:head>
+
+<section>
+	<h1>SIX<span>TO</span>M</h1>
+</section>
+
+<style>
+	section {
+		height: 100vh;
+		width: 100vw;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		background-color: black;
+		color: white;
+	}
+	h1 {
+		background-color: black;
+		color: white;
+		margin: 0;
+	}
+
+	span {
+		background-color: white;
+		color: black;
+		padding-left: 0.125rem;
+		margin-left: 0.125rem;
+		padding-top: 0.4rem;
+	}
+</style>
