5631d9ad2f267a76265bf2aabf98e3ce1beb0efa
Sam <57677512+threesam@users.noreply.github.com>
2024-12-04 22:07:29 -0500

    tries to get that route to run longer

--- svelte.config.js
+++ svelte.config.js
@@ -11,10 +11,6 @@ export default {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter(),
-
-		prerender: {
-			entries: ['*'] // Prerender all routes
-		}
+		adapter: adapter({ maxDuration: 60 })
 	}
 }
