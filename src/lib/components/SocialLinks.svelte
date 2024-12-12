062202d076321e6de1d7a38adc2de819f39c1ace
Sam <57677512+threesam@users.noreply.github.com>
2024-12-12 01:00:23 -0500

    updates social links

--- src/lib/components/SocialLinks.svelte
+++ src/lib/components/SocialLinks.svelte
@@ -5,11 +5,11 @@
 			title: 'Facebook'
 		},
 		linkedin: {
-			href: '#',
+			href: 'https://linkedin.com/in/threesam',
 			title: 'LinkedIn'
 		},
 		x: {
-			href: '#',
+			href: 'https://x.com/six_to_m',
 			title: 'X'
 		}
 	}
@@ -18,7 +18,7 @@
 </script>
 
 <div class="flex space-x-4 text-gray-100">
-	<a href={facebook.href} class="transition-all duration-300 hover:scale-95 hover:text-yellow-400">
+	<!-- <a href={facebook.href} class="transition-all duration-300 hover:scale-95 hover:text-yellow-400">
 		<span class="sr-only">{facebook.title}</span>
 		<svg class="h-16 w-16" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
 			<path
@@ -27,17 +27,17 @@
 				clip-rule="evenodd"
 			/>
 		</svg>
-	</a>
-	<a href={linkedin.href} class="transition-all duration-300 hover:scale-95 hover:text-yellow-400">
-		<span class="sr-only">{linkedin.title}</span>
+	</a> -->
+	<a href={x.href} class="transition-all duration-300 hover:scale-95 hover:text-yellow-400">
+		<span class="sr-only">{x.title}</span>
 		<svg class="h-16 w-16" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
 			<path
 				d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84"
 			/>
 		</svg>
 	</a>
-	<a href={x.href} class="transition-all duration-300 hover:scale-95 hover:text-yellow-400">
-		<span class="sr-only">{x.title}</span>
+	<a href={linkedin.href} class="transition-all duration-300 hover:scale-95 hover:text-yellow-400">
+		<span class="sr-only">{linkedin.title}</span>
 		<svg class="h-16 w-16" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
 			<path
 				fill-rule="evenodd"
